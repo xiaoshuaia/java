f33c84ebe74e19b40d94e038c4dfd7ab47dd9888
409635974@qq.com <409635974@qq.com>
2020-04-30 21:03:14 +0800

    123

--- xiaoshuai/src/day01/Javatest.java
+++ xiaoshuai/src/day01/Javatest.java
@@ -2,7 +2,9 @@ package day01;
 
 public class Javatest {
 public static void main(String[] args) {
-	System.out.println(123);
+	System.out.println(123);	System.out.println(123);	System.out.println(123);	System.out.println(123);
+	
+	
 }
 
 
